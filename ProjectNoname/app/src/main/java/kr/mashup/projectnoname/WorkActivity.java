cb5647e3977960d18bbf5b0cdab6de1cd9fd40ac
chaeeun <chaeeun218@gmail.com>
2017-11-05 04:49:12 +0900

    열람 화면 뷰 구현

--- ProjectNoname/app/src/main/java/kr/mashup/projectnoname/WorkActivity.java
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/WorkActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,6 +23,11 @@ public class WorkActivity extends AppCompatActivity {
     LinearLayout mSliderDotspannel;
     private int dotsCount;
     private ArrayList<ImageView> dots;
+    RecyclerView mRecyclerView;
+    ArrayList<FeedbackData> mDatas;
+    LinearLayoutManager mLayoutManager;
+    FeedbackViewAdapter mFeedbackViewAdapter;
+    TextView mSendTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +51,7 @@ public class WorkActivity extends AppCompatActivity {
             dots.get(i).setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
 
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(8,0,8,0);
+            params.setMargins(26,0,26,0);
             mSliderDotspannel.addView(dots.get(i),params);
 
         }
@@ -68,6 +77,32 @@ public class WorkActivity extends AppCompatActivity {
 
             }
         });
+
+
+        mRecyclerView= findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(false);
+        mRecyclerView.setNestedScrollingEnabled(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mDatas = new ArrayList<>();
+
+        mDatas.add(new FeedbackData("익명", "솔직히 이건 가나다라마바사 아자차카. 카카!! 가나다라 마밥일이삼사 오육 굿굿"));
+        mDatas.add(new FeedbackData("익명", "너무 초짜 티나는데요? 서점 가서 좀더.. 랄랄라 하"));
+        mDatas.add(new FeedbackData("익명", "상단의 레이아웃을 조금 더 가가가가나나 다다. ㅎㅎㅎ 가나 다라마바사 아자자 하하하하하 카카카카 하면 좋을 것 같아요!"));
+
+        mFeedbackViewAdapter = new FeedbackViewAdapter(mDatas);
+        mRecyclerView.setAdapter(mFeedbackViewAdapter);
+
+        mSendTextView = findViewById(R.id.sendTextView);
+        mSendTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRecyclerView.setVisibility(View.VISIBLE);
+            }
+        });
+
 
 
     }
