55bbcf82398203ee874a6dc6f68b77aec06a7871
jspiner <jspiner@naver.com>
2017-11-05 01:55:19 +0900

    fix mvp basefragment

--- ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/base/BaseFragment.java
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/base/BaseFragment.java
@@ -1,11 +1,22 @@
 package kr.mashup.projectnoname.ui.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public abstract class BaseFragment<T> extends Fragment {
 
     protected T presenter;
 
-    protected abstract void init();
+    protected abstract T buildPresenter();
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        presenter = buildPresenter();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
