ed5cbacac66d78d942b97dcc506cb09457c88836
jspiner <jspiner@naver.com>
2017-11-05 05:09:11 +0900

    #7 connect work activity

--- ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/main/tabs/creation/CreationViewHolder.java
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/main/tabs/creation/CreationViewHolder.java
@@ -1,10 +1,12 @@
 package kr.mashup.projectnoname.ui.main.tabs.creation;
 
+import android.content.Intent;
 import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import kr.mashup.projectnoname.R;
+import kr.mashup.projectnoname.WorkActivity;
 import kr.mashup.projectnoname.databinding.CardMainCreationBinding;
 import kr.mashup.projectnoname.model.Creation;
 
@@ -16,6 +18,10 @@ public class CreationViewHolder extends RecyclerView.ViewHolder {
         super(binding.getRoot());
 
         this.binding = (CardMainCreationBinding) binding;
+
+        binding.getRoot().setOnClickListener(__ -> {
+            startWorkActivity();
+        });
     }
 
     public void setData(Creation creation) {
@@ -32,6 +38,11 @@ public class CreationViewHolder extends RecyclerView.ViewHolder {
                         R.drawable.test_img_3
                 }[(int) (Math.random() * 3)]
         );
+    }
+
+    private void startWorkActivity() {
+        Intent intent = new Intent(binding.getRoot().getContext(), WorkActivity.class);
+        binding.getRoot().getContext().startActivity(intent);
     }
 
 }
