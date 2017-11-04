dbe2571b8e15a24e3a3fdf98f370a4f3e6dcfb11
jspiner <jspiner@naver.com>
2017-11-05 01:15:45 +0900

    add main page ui #4

---
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/main/tabs/creation/Contract.java
@@ -0,0 +1,13 @@
+package kr.mashup.projectnoname.ui.main.tabs.creation;
+
+public interface Contract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+}
---
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/main/tabs/creation/CreationFragment.java
@@ -0,0 +1,6 @@
+package kr.mashup.projectnoname.ui.main.tabs.creation;
+
+import kr.mashup.projectnoname.ui.base.BaseFragment;
+
+public class CreationFragment extends BaseFragment<Contract.View> implements Contract.Presenter{
+}
