5053c17373d6eb846dcddbe968230cc594271be2
jspiner <jspiner@naver.com>
2017-11-04 21:13:10 +0900

    fix review issues #3

--- ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/base/BaseActivity.java
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/base/BaseActivity.java
@@ -2,16 +2,10 @@ package kr.mashup.projectnoname.ui.base;
 
 import android.support.v7.app.AppCompatActivity;
 
-public class BaseActivity<T> extends AppCompatActivity {
+public abstract class BaseActivity<T> extends AppCompatActivity {
 
     protected T presenter;
 
-    public void setPresenter(T presenter) {
-        this.presenter = presenter;
-    }
-
-    public T getPresenter() {
-        return presenter;
-    }
+    protected abstract void init();
 
 }
--- ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/base/BaseFragment.java
+++ ProjectNoname/app/src/main/java/kr/mashup/projectnoname/ui/base/BaseFragment.java
@@ -2,16 +2,10 @@ package kr.mashup.projectnoname.ui.base;
 
 import android.support.v4.app.Fragment;
 
-public class BaseFragment<T> extends Fragment {
+public abstract class BaseFragment<T> extends Fragment {
 
     protected T presenter;
 
-    public void setPresenter(T presenter) {
-        this.presenter = presenter;
-    }
-
-    public T getPresenter() {
-        return presenter;
-    }
+    protected abstract void init();
 
 }
