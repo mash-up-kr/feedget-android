84ad18f8bd84e4751dee63f9ef4eaf36e7bc8ea1
Eastroots92DESKTOP <eastroots92@gmail.com>
2017-12-23 02:48:39 +0900

    RegisterPointActivity 추가 및 등록

--- ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/register/RegisterPointActivity.java
+++ ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/register/RegisterPointActivity.java
@@ -1,8 +1,25 @@
 package kr.mashup.feedget.ui.register;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import kr.mashup.feedget.R;
+
+import kr.mashup.feedget.databinding.ActivityRegisterPointBinding;
+
 /**
  * Created by eastroots92 on 2017-12-23.
  */
 
-public class RegisterPointActivity {
+public class RegisterPointActivity extends AppCompatActivity{
+
+    private ActivityRegisterPointBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register_point);
+
+    }
 }
