d11aac6338d25f9b7f37740436fa6e97a42b372a
Omjoonkim <omjoonkim@gmail.com>
2017-12-23 00:46:35 +0900

    Merge branch 'feature/#20' into feature/middle
    # Conflicts:
    #	ProjectFeedget/app/src/main/AndroidManifest.xml
    #	ProjectFeedget/app/src/main/java/kr/mashup/feedget/model/Creation.java
    #	ProjectFeedget/app/src/main/res/layout/toolbar_base.xml
    #	ProjectFeedget/app/src/main/res/values/colors.xml

---
+++ ProjectFeedget/app/src/main/java/kr/mashup/feedget/model/Creation.java
@@ -0,0 +1,18 @@
+package kr.mashup.feedget.model;
+
+import java.io.Serializable;
+
+public class Creation implements Serializable {
+
+    public int creationId;
+    public String description;
+    public int feedbackCount;
+    public String title;
+    public String duedate;
+
+
+    public Creation(){
+
+    }
+
+}
