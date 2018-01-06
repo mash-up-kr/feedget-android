ac660cabfc2c0f421f93e7dead4006541fe746d8
jspiner <jspiner@naver.com>
2018-01-06 14:16:34 +0900

    add screen orientation, add databinding in creationdetail

--- ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/creation/detail/CreationDetailActivity.java
+++ ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/creation/detail/CreationDetailActivity.java
@@ -1,14 +1,19 @@
 package kr.mashup.feedget.ui.creation.detail;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import kr.mashup.feedget.R;
+import kr.mashup.feedget.databinding.ActivityCreationDetailBinding;
+import kr.mashup.feedget.model.dummy.Creation;
 import kr.mashup.feedget.ui.base.BaseActivity;
 import kr.mashup.feedget.util.IntentKey;
 
 public class CreationDetailActivity extends BaseActivity<CreationDetailPresenter> implements Contract.View {
+
+    private ActivityCreationDetailBinding binding;
 
     @Override
     protected CreationDetailPresenter buildPresenter() {
@@ -18,7 +23,7 @@ public class CreationDetailActivity extends BaseActivity<CreationDetailPresenter
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creation_detail);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_creation_detail);
 
         presenter.init();
     }
@@ -29,5 +34,10 @@ public class CreationDetailActivity extends BaseActivity<CreationDetailPresenter
         Intent intent = new Intent();
         intent.putExtra(IntentKey.INTENT_KEY_CREATION_ID, "testId");
         return intent;
+    }
+
+    @Override
+    public void setCreation(Creation creation) {
+        binding.setCreation(creation);
     }
 }
--- ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/creation/detail/CreationDetailPresenter.java
+++ ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/creation/detail/CreationDetailPresenter.java
@@ -9,7 +9,7 @@ public class CreationDetailPresenter implements Contract.Presenter {
 
     private Contract.View view;
 
-    public CreationDetailPresenter(Contract.View view){
+    public CreationDetailPresenter(Contract.View view) {
         this.view = view;
     }
 
@@ -20,11 +20,11 @@ public class CreationDetailPresenter implements Contract.Presenter {
         );
     }
 
-    private void requestCreationDetail(String creationId){
+    private void requestCreationDetail(String creationId) {
         // TODO : 더미데이터가 아닌 실 데이터를 부르도록 수정
         DummyApi.getInstance().getCreationDetail(creationId).subscribe(
                 creation -> {
-                    Log.i(CreationDetailPresenter.class.getSimpleName(), "creation : " + creation);
+                    view.setCreation(creation);
                 }
         );
     }
