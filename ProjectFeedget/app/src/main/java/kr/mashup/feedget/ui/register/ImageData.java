43b24d5761595f479642016ca18a0316af45b562
Eastroots92 <eastroots92@gmail.com>
2018-01-13 20:54:58 +0900

    DataClass 추가

--- ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/register/ImageData.java
+++ ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/register/ImageData.java
@@ -1,8 +1,10 @@
 package kr.mashup.feedget.ui.register;
 
+import java.io.Serializable;
+
 /**
  * Created by eastroots92 on 2018. 1. 13..
  */
 
-public class ImageData {
+public class ImageData implements Serializable {
 }
