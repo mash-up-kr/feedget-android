74d99c60165aede641d98ff5ee2cfeae44c903e3
Eastroots92DESKTOP <eastroots92@gmail.com>
2018-03-11 03:41:42 +0900

    이미지 보관하는 Data Class 생성

--- ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/register/ImageData.java
+++ ProjectFeedget/app/src/main/java/kr/mashup/feedget/ui/register/ImageData.java
@@ -1,10 +1,36 @@
 package kr.mashup.feedget.ui.register;
 
+import android.net.Uri;
+
 import java.io.Serializable;
+
+import java.util.ArrayList;
 
 /**
  * Created by eastroots92 on 2018. 1. 13..
  */
 
 public class ImageData implements Serializable {
+    private String creationId;
+    private ArrayList<Uri> files;
+
+    public String getCreationId() {
+        return creationId;
+    }
+
+    public void setCreationId(String creationId) {
+        this.creationId = creationId;
+    }
+
+    public ArrayList<Uri> getFiles() { return files;}
+
+    public void setFiles(Uri file) { this.files.add(file); }
+
+    public boolean isImage(){
+        if(getFiles().isEmpty()){
+            return false;
+        }else {
+            return true;
+        }
+    }
 }
